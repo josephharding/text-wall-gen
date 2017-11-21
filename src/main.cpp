4e638b9d3da21185435b87294fb0b6a5571a6c0f
Joe Harding <joe.harding@us.ibm.com>
2017-11-21 14:48:12 -0600

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,23 @@
 int main (int argc, char *argv[])
 {
   YAML::Node config = YAML::LoadFile("../config.yaml"); // relative to executable location in build
+  
+	Magick::Image pix_image(Magick::Geometry(300, 100), Magick::Color("transparent"));
 
-  if (config["text"])
-	{ std::cout << "text: " << config["text"].as<std::string>() << "\n";
+  pix_image.strokeColor(Magick::Color("red"));
+  pix_image.fontPointsize(20);
+
+  int y = 20; 
+  for(unsigned i = 0; i < config["text"].size(); i++)
+  {
+    std::string myst = config["text"][i].as <std::string> ();
+    std::cout << "Found scalar: " << config["text"][i] << std::endl;
+	  pix_image.draw(Magick::DrawableText(0, y, config["text"][i].as <std::string> ()) );
+    y += 20;
   }
-
-	Magick::Image pix_image(Magick::Geometry(100, 100), Magick::Color("white"));;
-	pix_image.draw(Magick::DrawableText(0, 50, config["text"].as<std::string>()) );
-	pix_image.magick("BMP2");
-	pix_image.write("one.bmp");
+	
+	pix_image.magick("png");
+	pix_image.write("one.png");
 
   return 0;
 }
